d3261b22fdc64658f968b68dcbd02a6d28d9adcf
Razvan-Marian-Budurovici <112555360+Razvan-Marian-Budurovici@users.noreply.github.com>
2024-10-10 11:35:35 +0200

    Json reading test

--- Source/SIC-Integration/src/main/java/org/example/Main.java
+++ Source/SIC-Integration/src/main/java/org/example/Main.java
@@ -4,8 +4,19 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+/*
+Commands:
+docker cp test.json spark-master:/opt/bitnami/spark/
+docker cp target/SIC-Integration-1.0-SNAPSHOT.jar spark-master:/opt/bitnami/spark/
+docker exec -it spark-master bash
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --class org.example.Main /opt/bitnami/spark/SIC-Integration-1.0-SNAPSHOT.jar
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SparkSession spark = SparkSession.builder().appName("StreamingTest").master("local[*]").getOrCreate();
+
+        Dataset<Row> df = spark.read().option("multiline","true").json("/opt/bitnami/spark/test.json");
+        df.show();
     }
 }
