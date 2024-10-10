7678b6a004b3ac53051a8bccc24086a49ff56160
Razvan-Marian-Budurovici <112555360+Razvan-Marian-Budurovici@users.noreply.github.com>
2024-10-10 09:39:05 +0200

    Project rename

--- Source/SIC-Integration/src/main/java/org/example/Main.java
+++ Source/SIC-Integration/src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
