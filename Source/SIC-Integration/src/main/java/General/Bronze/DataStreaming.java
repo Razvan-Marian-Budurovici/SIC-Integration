98acb41adf66020d6a8aa48a112150c2164a08c3
Razvan-Marian-Budurovici <112555360+Razvan-Marian-Budurovici@users.noreply.github.com>
2024-10-31 14:53:47 +0100

    Added simple generic streaming function

---
+++ Source/SIC-Integration/src/main/java/General/Bronze/DataStreaming.java
@@ -0,0 +1,35 @@
+package General.Bronze;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class DataStreaming {
+
+    /*
+    General Structured streaming function for streaming files from pulsar
+    Should stay active once run, or until spark application termination
+
+    spark - the spark session object
+    serviceUrl - link to pulsar service url ex:"pulsar://localhost:6650"
+    adminUrl - link to admin url ex:"http://localhost:8080"
+    topic - identifier of the message stream within pulsar ex:"persistent://tenant/namespace/topic-name"
+    outputPath - path to output of parquet files converted from stream
+    checkpointPath - path to checkpoint location (needed in case of errors)
+    */
+    public void dataStream (SparkSession spark, String serviceUrl, String adminUrl, String topic, String outputPath, String checkpointPath) throws Exception{
+        Dataset<Row> fileStream = spark.readStream()
+                .format("pulsar")
+                .option("service.url", serviceUrl)
+                .option("admin.url", adminUrl)
+                .option("topic", topic)
+                .load();
+
+        fileStream.writeStream()
+                .format("parquet")
+                .option("path", outputPath)
+                .option("checkpointLocation", checkpointPath)
+                .start()
+                .awaitTermination();
+    }
+}
